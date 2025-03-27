0b7ba5d5c7472ec1fa9af52879cfaac373ce8558
andy <nospam@privacy.org>
2025-03-27 12:05:08 -0700

    update running time

--- cat-soup-machine.ino
+++ cat-soup-machine.ino
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 
-const uint32_t RUN_TIME = 20e3;
-const uint32_t INTERVAL = 60e3;
+const uint32_t RUN_TIME = 35e3;
 uint32_t lastMillis = 0;
 uint32_t now = 0;
 
@@ -9,25 +8,20 @@ void setup(){
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(2, INPUT);
   Serial.begin(9600);
+  Serial.println("start");
 }
 
 void loop(){
   now = millis();
 
-  // overflow
-  if (now < lastMillis){
-    lastMillis = now;
-  }
-
-  // if (digitalRead(2) == HIGH){
-  if (Serial.available() > 0 
-      && Serial.read() == 'x'
-      && (now - lastMillis > INTERVAL || lastMillis == 0)){
+  if (digitalRead(2) == HIGH){
     lastMillis = now;
     Serial.println("running");
+
     digitalWrite(LED_BUILTIN, HIGH); 
     delay(RUN_TIME);
     digitalWrite(LED_BUILTIN, LOW);
+
     Serial.println("done");
     Serial.println(millis());
   }
