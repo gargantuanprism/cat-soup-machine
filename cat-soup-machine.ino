a9fd65d45d60d5bb049fbc13cdf47c17479d03e4
andy <nospam@privacy.org>
2025-05-13 22:35:24 -0700

    run pump based on length of motor activation

--- cat-soup-machine.ino
+++ cat-soup-machine.ino
@@ -1,29 +1,51 @@
 #include <Arduino.h>
 
-const uint32_t RUN_TIME = 35e3;
-uint32_t lastMillis = 0;
+const uint8_t BUTTON_PIN = 2;
+const uint32_t LONG_PRESS_TIME = 500;
+const uint32_t PUMP_TIME_FACTOR = 5;
+uint32_t startMillis = 0;
 uint32_t now = 0;
+uint8_t state = 0;
+uint8_t prevState = 0;
 
 void setup(){
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(2, INPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  digitalWrite(LED_BUILTIN, LOW);
+
   Serial.begin(9600);
   Serial.println("start");
 }
 
 void loop(){
   now = millis();
+  state = digitalRead(BUTTON_PIN);
 
-  if (digitalRead(2) == HIGH){
-    lastMillis = now;
-    Serial.println("running");
+  if (state != prevState){
 
-    digitalWrite(LED_BUILTIN, HIGH); 
-    delay(RUN_TIME);
-    digitalWrite(LED_BUILTIN, LOW);
+    // pressed, start timer
+    if (state == LOW){
+      Serial.println("pressed");
+      startMillis = millis();
+    }
 
-    Serial.println("done");
-    Serial.println(millis());
+    // released, stop timer
+    else if (state == HIGH && now - startMillis > LONG_PRESS_TIME){
+      uint32_t pressTime = now - startMillis;
+      uint32_t runTime = pressTime * PUMP_TIME_FACTOR;
+
+      Serial.print("pressed for: ");
+      Serial.println(pressTime);
+      Serial.print("run time: ");
+      Serial.println(runTime);
+
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(runTime);
+      digitalWrite(LED_BUILTIN, LOW);
+    }
   }
+
+  prevState = state;
 }
 
